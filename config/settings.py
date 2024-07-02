6842defc1d125d148812832b65c35df02250857c
eskimosa <esskimosa@gmail.com>
2024-07-02 20:00:33 +0200

    fixed static files issue

--- config/settings.py
+++ config/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where static files will be collected
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
