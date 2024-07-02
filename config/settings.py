d53ba8b3aa0bafc71312ef0fef1b0866f27d6a96
eskimosa <esskimosa@gmail.com>
2024-07-02 23:07:56 +0200

    fixed static files issue v9

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import logging
+from django.core.management.utils import get_random_secret_key
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
