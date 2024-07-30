fd349a6e8c0ee3090554a3021c8c99f8b3c389de
eskimosa <esskimosa@gmail.com>
2024-07-30 20:50:36 +0200

    working on cors settings for domain

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 
 ]
 
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://news-management.vritat.com:8000,http://news-management.vritat.com:3001,http://localhost:3000,http://localhost:3001,http://13.51.155.141:3001').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://news-management.vritat.com:8000,http://news-management.vritat.com:3001,http://localhost:3000,http://localhost:3001,http://13.51.155.141:3001,http://13.51.155.141:8000').split(',')
 
 
 CORS_ALLOW_HEADERS = [
@@ -99,7 +99,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://news-management.vritat.com:8000,http://news-management.vritat.com:3001,http://localhost:3000,http://localhost:3001,http://13.51.155.141:3001').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://news-management.vritat.com:8000,http://news-management.vritat.com:3001,http://localhost:3000,http://localhost:3001,http://13.51.155.141:3001,http://13.51.155.141:8000').split(',')
 
 ROOT_URLCONF = 'config.urls'
 
